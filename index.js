1273441a22cc8d72e2d05fb35c9c503029786984
Kamryn Smith <programersign@gmail.com>
2023-03-14 11:59:54 -0700

    assignment complete

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
 /* Your Code Here */
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+// Your code here
+function createEmployeeRecord(array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+function createEmployeeRecords(array) {
+    return array.map(function(arr) {
+        return createEmployeeRecord(arr)
+    });
+}
+
+function createTimeInEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(" ");
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date: `${date}`
+    })
+    return this
+}
+
+function createTimeOutEvent(dateStamp) {
+    let [date, hour] = dateStamp.split(" ");
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date: `${date}`
+    })
+    return this
+}
+
+function hoursWorkedOnDate(formDate) {
+    let dateSoughtIn = this.timeInEvents.find(function(element) {
+        if (element.date === formDate) {
+            return element.date
+        }
+    })
+
+    let dateSoughtOut = this.timeOutEvents.find(function(element) {
+        if (element.date === formDate) {
+            return element.date
+        }
+    })
+
+    let hours1 = dateSoughtOut.hour - dateSoughtIn.hour;
+    return hours1 / 100
+}
+
+function wagesEarnedOnDate(formDate) {
+    let payOwed = hoursWorkedOnDate.call(this, formDate) * this.payPerHour;
+    return parseFloat(payOwed)
+}
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
@@ -19,5 +70,19 @@ const allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function findEmployeeByFirstName(srcArray, firstNameString) {
+    return srcArray.find(function(element) {
+        if (element.firstName === firstNameString) {
+            return firstNameString
+        }
+    })
+}
+
+function calculatePayroll(arrayEmployeeRec) {
+    return arrayEmployeeRec.reduce(function(accumulator, currentValue) {
+        return accumulator + allWagesFor.apply(currentValue)
+    }, 0)
 }
 
